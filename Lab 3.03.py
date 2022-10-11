5a351d0edc92e3d13c94d3850cfd7c07853a6930
Blake Harrington <baharrington25@students.bmchs.org>
2022-10-11 14:48:31 -0500

    Lab 3.03.py

--- Lab 3.03.py
+++ Lab 3.03.py
@@ -223,30 +223,14 @@ Add on when finished with above:
 Instead of closing the program when the deck is empty, create a way for the user to play again.
 '''
 
-
 import random
+
+from pkg_resources import parse_version
 
 def shuffled_deck():
     basic_deck = list(range(1,14)) * 4
     random.shuffle(basic_deck)
     return basic_deck
-
-#Full Deck
-deck = shuffled_deck()
-
-#Split Between Players
-player_1_deck = deck[0:26]
-player_2_deck = deck[26:53]
-
-#Player Scores
-player_1_score = 0
-player_2_score = 0
-
-#Player Names
-player_1 = input("What is your name, player 1?")
-player_2 = input("What is your name, player 2?")
-current_player = player_1
-current_deck = player_1_deck
 
 #Player Drawing
 "Have a function player_turn, with the contract shown below:"
@@ -255,28 +239,90 @@ current_deck = player_1_deck
 # Arguments: player_name as string, deck as list
 # returns: integer
 
+# Compares Scores and Returns Player Who Won
+def greater_card(p1_card, p2_card):
+    global points_available
+    global player_1_score, player_2_score
+    if p1_card > p2_card:
+        if points_available > 2:
+            print(f"{player_2_name} wins the war of {points_available} cards!")
+            player_1_score += points_available
+            points_available = 2
+            return f"{player_1_name}: {player_1_score}   {player_2_name}: {player_2_score}"
+        else:
+            print( f"{player_1_name} wins the cards! ")
+            player_1_score += points_available
+            points_available = 2
+            return f"{player_1_name}: {player_1_score}   {player_2_name}: {player_2_score}"
+    elif p2_card > p1_card:
+        if points_available > 2:
+            print(f"{player_2_name} wins the war of {points_available} cards!")
+            player_2_score += points_available
+            points_available = 2
+            return f"{player_1_name}: {player_1_score}   {player_2_name}: {player_2_score}" 
+        else:
+            print (f"{player_2_name} wins the cards! ")
+            player_2_score += points_available
+            points_available = 2
+            return f"{player_1_name}: {player_1_score}   {player_2_name}: {player_2_score}" 
+    elif player_1_draw == player_2_draw:
+        points_available += 2
+        return "War"
+
+
+
 def player_turn(player_1, player_2):
-    player_1_draw = player_1_deck.pop(0)
-    player_2_draw = player_2_deck.pop(0)
+    global player_1_draw
+    global player_2_draw
+    global current_player
     if current_player == player_1:
         current_player = player_2
-        current_deck = player_2
         return f"{player_1} drew a {player_1_draw}!"
     else:
         current_player = player_1
-        current_deck = player_1
         return f"{player_2} drew a {player_2_draw}!"
-        
-running = True
-while running:
-    print(f"{player_1}, it is your turn! ")
 
-    input()
 
-    if current_player == player_1:
-        print(player_turn(current_player,current_deck))
-        current_player = player_2
-        current_deck = player_2
-    elif current_deck == player_2:
-        current_player = player_1
-        current_deck = player_1
+
+#Full Deck
+deck = shuffled_deck()
+points_available = 2
+
+#Split Between Players
+player_1_deck = deck[0:26]
+player_2_deck = deck[26:53]
+
+
+
+#Player Scores
+player_1_score = 0
+player_2_score = 0
+
+#Player Names
+player_1_name = input("What is your name, player 1? ")
+player_2_name = input("What is your name, player 2? ")
+current_player = player_1_name
+current_deck = player_1_deck
+
+
+
+
+
+running = True         
+while len(player_2_deck):
+
+    print()
+    player_1_draw = player_1_deck.pop(0)
+    player_2_draw = player_2_deck.pop(0)
+    print(player_turn(player_1_name, player_2_name))
+    print(player_turn(player_1_name, player_2_name))
+    print(greater_card(player_1_draw, player_2_draw))
+    input("Press ENTER to continue to the next round: ")
+
+print()
+if player_1_score > player_2_score:
+    print(f"{player_1_name} has won! ")
+elif player_2_score > player_1_score:
+    print(f"{player_2_name} has won! ")
+else:
+    print("There was a tie, nobody wins! ")
