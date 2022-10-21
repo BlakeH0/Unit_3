0aa862b279761893d09e0264d0437e29d2f5fe62
Blake Harrington <baharrington25@students.bmchs.org>
2022-10-21 14:48:20 -0500

    Fall2022_OregonTrail.py

---
+++ Fall2022_OregonTrail.py
@@ -0,0 +1,15 @@
+# Blake Harrington
+# 21 October, 2022
+# Oregon Trail
+
+
+
+# Name
+user_name = input("What is your name? ")
+
+
+running = True
+while running:
+
+    # Player Decision
+    input("What would you like to do: travel, rest, hunt, status, help, quit?  ")
